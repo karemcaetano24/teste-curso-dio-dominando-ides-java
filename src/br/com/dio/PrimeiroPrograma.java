a5388802e5a38457feda04bca825fb1204a73843
Karem Caetano <karemcaetano@gmail.com>
2022-10-15 21:41:42 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato ();
+	    Livros Livros = new Livros();
+		System.out.println(gato);
+		System.out.println(Livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String  npag;
+}
